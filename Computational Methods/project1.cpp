bb8f9682df5d433d3836b4bd067fd0d1f3ac47f4
sujunhao <sujunhao.hit@gmail.com>
2015-03-17 16:57:36 +0800

    update
    update

--- Computational Methods/project1.cpp
+++ Computational Methods/project1.cpp
@@ -10,7 +10,7 @@ using namespace std;
 double num[MAX], x, y, xx[MAX];
 int  n=1, k, xn, nn;
 
-double fun(const double x, int n, double num[]) //Lagrange插值 num[]放n个已知点， 1~n/2为xi,n/2+1~n为yi x为自变量
+double Lagrange(const double x, int n, double num[]) //Lagrange插值 num[]放n个已知点， 1~n/2为xi,n/2+1~n为yi x为自变量
 {
     double y=0;
     for (int i=1; i<=n/2; i++){
@@ -56,7 +56,7 @@ int main()
         num[i+2+k]=get(num[i+1]);
         //printf("%d %lf %lf\n", i+1, num[i+1], num[i+2+k]);
     }
-    y=fun(x, 2*k+2, num);
+    y=Lagrange(x, 2*k+2, num);
     printf("x=%lf y=%lf\n", x, y);
     }
     }
